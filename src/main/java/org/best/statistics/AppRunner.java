73753402f3e45160c4b8b54726e6ce71861000ec
yangfan <347635191@qq.com>
2026-01-23 22:16:20 +0800

    init repo

---
+++ src/main/java/org/best/statistics/AppRunner.java
@@ -0,0 +1,13 @@
+package org.best.statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class AppRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(AppRunner.class, args);
+    }
+}
